cd4da02d4b096f5724ec5da14046acbdf5aa532c
cheolhakja <rebegin15@gmail.com>
2023-05-25 17:30:36 +0900

    사용자 입력 구현

--- ch5/전화번호부실습/전화번호부.py
+++ ch5/전화번호부실습/전화번호부.py
@@ -1,10 +1,22 @@
 def main_menu():
-    menu = int
+    menu = int(input("1에서 5사이의 수를 입력하시오"))
 
 
 while (selected := main_menu()) != 5:  # 5는 종료 메뉴
 
-    pass
+    if(selected == 1):
+
+
+        pass
+
+    elif(selected == 2):
+        pass
+    elif(selected == 3):
+        pass
+    elif(selected == 4):
+        pass
+    else:
+        pass
 
 else:
     # 종료하기 전 파일로 데이터 저장
